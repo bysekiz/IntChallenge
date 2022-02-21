5a5b91393bcecdc1490d149270111399c9531aa2
Korzay <korzay.aksu@gmail.com>
2022-02-21 14:50:14 +0300

    1 Billion Integer Numbers 1 sec

---
+++ IntChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            var threadList = new List<Thread>();
+            try
+            {
+                while (true)
+                {
+                    Thread newThread = new Thread(new ThreadStart(Write), 1024);
+                    newThread.Start();
+                    threadList.Add(newThread);
+                    count++;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public static void Write()
+        {
+            for (int k = 1; k < 1000; k++)
+            {
+                Console.WriteLine(k);
+
+            }
+            Console.ReadKey();
+            
+        }
+    }
+   
+}
